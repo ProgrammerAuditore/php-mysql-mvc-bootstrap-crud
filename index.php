071b336265b2ae78f19e6c086b62c4a6562761ff
ProgrammerAuditore <victorvj098@gmail.com>
2021-09-30 17:36:33 -0500

    Recibir valore para mostrar un controlador y una vista

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-  require_once("./vistas/template.php");
+
+if( isset($_GET['controlador']) && isset($_GET['accion']) ){
+  
+  $controlador = $_GET['controlador'];
+  $accion = $_GET['accion'];
+
+  
+
+}
+
+require_once("vistas/template.php");
 ?>
