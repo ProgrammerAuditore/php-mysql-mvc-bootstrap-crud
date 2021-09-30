cfc0ae2748328f49b7f32dbc3802871225b9874c
ProgrammerAuditore <victorvj098@gmail.com>
2021-09-30 17:56:25 -0500

    *** validar controladores y acciones nulos o vacios

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 
-if( isset($_GET['controlador']) && isset($_GET['accion']) ){
-  
-  $controlador = $_GET['controlador'];
-  $accion = $_GET['accion'];
+$controlador="paginas";
+$accion="inicio";
 
-  
+if( isset($_GET['controlador']) && isset($_GET['accion']) ){
+
+  if( ($_GET['controlador'] != "") && ($_GET['accion'] != "") ){
+    $controlador = $_GET['controlador'];
+    $accion = $_GET['accion'];
+  }
 
 }
 
