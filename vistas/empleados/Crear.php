022327253f6342aba58fc6293d629a2437dc19d4
ProgrammerAuditore <victorvj098@gmail.com>
2021-09-30 18:11:19 -0500

    *** Se agrego contenido en la vista Crear y Inicio empleados

--- vistas/empleados/Crear.php
+++ vistas/empleados/Crear.php
@@ -1,1 +1,21 @@
-Formulario de crear
+<div class="card">
+    <div class="card-header">
+        Agregar empleado
+    </div>
+    <div class="card-body">
+        <form action="" method="POST">
+            <div class="mb-3">
+              <label for="nombre" class="form-label">Nombre:</label>
+              <input type="text"
+                class="form-control" name="nombre" id="nombre" placeholder="Nombre del empleado">
+            </div>
+
+            <div class="mb-3">
+              <label for="correo" class="form-label">Correo: </label>
+              <input type="email" class="form-control" name="correo" id="correo" placeholder="Correo del empleado">
+            </div>
+
+            <input name="" id="" class="btn btn-success" type="button" value="Agregar empleado">
+        </form>
+    </div>
+</div>
--- vistas/empleados/Inicio.php
+++ vistas/empleados/Inicio.php
@@ -1,1 +1,18 @@
-Lista de empleados
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Oscar</td>
+            <td>example@example.com</td>
+            <td>Editar | Borrar</td>
+        </tr>
+    </tbody>
+</table>
